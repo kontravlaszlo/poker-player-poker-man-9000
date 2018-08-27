a5f18f2486b44061643dfbbd8483cd185573a8d2
HajnalFeri <kalacsf1@gmail.com>
2018-08-27 14:02:16 +0200

    log community object

--- Player.js
+++ Player.js
@@ -16,11 +16,11 @@ class Player {
     console.log(gameState.players[5].name);
 
     var mycards = gameState.players[5].hole_cards;
-    var mycardsJson = JSON.stringify(mycards);
+    //var mycardsJson = JSON.stringify(mycards);
     var communityCards = gameState.community_cards;
     var stack = gameState.players[5].stack;
     
-    console.log(mycardsJson);
+    console.log(mycards);
     console.log(communityCards);
     console.log(stack);
 
@@ -50,19 +50,19 @@ class Player {
 
     try {
       if (communityCards.size != 0) {
-        var communityObject = JSON.parse(communityCards);
-        console.log('Ezzzzzzzzzzzzzzz: ' + communityObject);
+        //var communityObject = JSON.parse(communityCards);
+        console.log('Ezzzzzzzzzzzzzzz: ' + communityCards);
       }
 
 
-      /*var cardsHandTableList = [];
+      var cardsHandTableList = [];
       for(i=0; i<mycards.size; i++) {
         cardsHandTableList.push(mycards[i].rank);
       }  
       for(i=0; i<communityObject.size; i++) {
       cardsHandTableList.push(communityObject[i].rank);
       }
-      console.log(cardsHandTableList);*/
+      console.log(cardsHandTableList);
     } catch(err) {
       console.log(err);
     }
