c2c9363dd50c6cedcdae69e839523c259656e7e2
Kontra László <kontravlaszlo@gmail.com>
2018-08-27 10:42:26 +0200

    commented out gamestate console log

--- Player.js
+++ Player.js
@@ -5,12 +5,8 @@ class Player {
 
   static betRequest(gameState, bet) {
     bet(0);
-<<<<<<< HEAD
-    console.log(gameState.Player);
-=======
-    console.log(gameState);
+    //console.log(gameState);
     console.log(Player.name);
->>>>>>> a0ab3dd01d0d3a099add1e7e5cba66922ed3b02e
   }
 
   static showdown(gameState) {
