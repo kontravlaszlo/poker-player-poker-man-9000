356fcfb8d50bd73bbd7a191fdedb89d332646825
dnorbert4000 <dnorbert@gmail.com>
2018-08-27 12:08:33 +0200

    new list for cards 3

--- Player.js
+++ Player.js
@@ -39,7 +39,7 @@ class Player {
     for(i=0; i<communityObject.size; i++) {
     cardsHandTableList.push(communityObject[i].rank);
     }
-    console.log(JSON.stringify(cardsHandTableList));
+    console.log(cardsHandTableList);
   }
 
   static showdown(gameState) {
