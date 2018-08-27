3edbb5092c0efaab8f170a3a17b3b0f7d7040eb6
dnorbert4000 <dnorbert@gmail.com>
2018-08-27 11:16:00 +0200

    hole cards3

--- Player.js
+++ Player.js
@@ -8,7 +8,7 @@ class Player {
     //console.log(gameState);
     console.log(gameState.players[4].name);
 
-    const mycards = gameState.players[5].hole_cards
+    var mycards = gameState.players[5].hole_cards
     mycards = JSON.stringify(mycards);
     console.log(mycards);
   }
